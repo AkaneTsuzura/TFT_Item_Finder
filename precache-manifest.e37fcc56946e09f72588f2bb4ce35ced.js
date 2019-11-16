b0c5fa308a031f6ecdb0ba78e3aab73a9703c5c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-16 11:20:46 +0000

    Deploy AkaneTsuzura/TFT_Item_Finder to github.com/AkaneTsuzura/TFT_Item_Finder.git:gh-pages

---
+++ precache-manifest.e37fcc56946e09f72588f2bb4ce35ced.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecef37e23e50bc07666c902f4d7304c",
+    "url": "/TFT_Item_Finder/index.html"
+  },
+  {
+    "revision": "828462648603afe1282c",
+    "url": "/TFT_Item_Finder/static/js/2.d820a211.chunk.js"
+  },
+  {
+    "revision": "d4b6e468ee2f3bc80278",
+    "url": "/TFT_Item_Finder/static/js/main.f2273b04.chunk.js"
+  },
+  {
+    "revision": "afd0fad11f27f053a48b",
+    "url": "/TFT_Item_Finder/static/js/runtime-main.42d3aa34.js"
+  }
+]);
